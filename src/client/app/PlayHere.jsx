60c7b013106db1fe98cc102a8577ddc60257af42
supritkumar shah <suprit7@gmail.com>
2016-07-21 12:21:45 -0400

    Changes to playhere and stay here components

--- src/client/app/PlayHere.jsx
+++ src/client/app/PlayHere.jsx
@@ -2,9 +2,23 @@ import React    from 'react'
 
 export default class PlayHere extends React.Component{
   render(){
+
     return(
-      <div>
-        <p> PlayHere </p>
+      <div className="list-group">
+        {Object.keys(this.props.play)
+      .map(key=>(
+        <div className="card" key={key}>
+            <img src={this.props.play[key].imageUrl}  className="card-img-left"/>
+            <div className="card-block">
+              <h4 className="card-title">{this.props.play[key].title}</h4>
+              <h4 className="card-title">{this.props.play[key].fromPrice}</h4>
+
+              <a href="#" className="btn btn-default"> Add to Plan</a>
+            </div>
+
+        </div>
+      ))
+    }
       </div>
       )
   }
