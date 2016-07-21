dec5f12d7a4c59a2765926961e834af797938fc0
Sasha-Shae Weekes <lovesweetshae13@gmail.com>
2016-07-21 18:51:53 -0400

    Updating Totals Component

--- src/client/app/StayHere.jsx
+++ src/client/app/StayHere.jsx
@@ -6,16 +6,15 @@ export default class StayHere extends React.Component{
     return(
       <div className="list-group">
         {Object.keys(this.props.places)
-      .map(key=>(
+      .map((key,index)=>(
         <div className="card" key={key}>
             <img src={this.props.places[key].picture}  className="card-img-left"/>
             <div className="card-block">
               <h4 className="card-title">{this.props.places[key].name}</h4>
               <h4 className="card-title">{this.props.places[key].averageRate}</h4>
               <p className="card-text">{this.props.places[key].fullAddress}</p>
-              <a href="#" className="btn btn-default"> Add to Plan</a>
+              <a id={index} onClick={this.props.updateStayTotal} className="btn btn-default"> Add to Plan</a>
             </div>
-
         </div>
       ))
     }
